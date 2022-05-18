66004840303e46f91b87635b9060f2c3b401a61f
Timur022 <61280530+Timur022@users.noreply.github.com>
2022-05-18 15:14:22 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+try {
+    $dbh = new PDO('pgsql:host=ec2-44-195-169-163.compute-1.amazonaws.com;port=5432;dbname=d9ok3cqiodrttb;', 'pcapbbwejsukfu', '4b1e4e19d50d23c36aade2fe7a6bc048f7affb9c50efb152e6c96904d9711489');
+ } catch (PDOException $e) {
+    print "Error!: " . $e->getMessage();
+    die();
+ }
