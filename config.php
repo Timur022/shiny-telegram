3dec28af8abc235f977d6c3f7e8fc855c6080b88
Timur022 <61280530+Timur022@users.noreply.github.com>
2022-05-31 20:21:35 +0500

    Add files via upload

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
-<?php 
-try {
-    $dbh = new PDO('pgsql:host=ec2-44-195-169-163.compute-1.amazonaws.com;port=5432;dbname=d9ok3cqiodrttb;', 'pcapbbwejsukfu', '4b1e4e19d50d23c36aade2fe7a6bc048f7affb9c50efb152e6c96904d9711489');
+<?php try {
+    $dbh = new PDO('pgsql:host=ec2-52-30-67-143.eu-west-1.compute.amazonaws.com;port=5432;dbname=ddve8o07jnqq2j;', 'ddzzglpcjdrdau', '0649ced53a2998fdff690fb107b3990d5cb8a847a45f3d62b38441f11661e154');
  } catch (PDOException $e) {
     print "Error!: " . $e->getMessage();
     die();
