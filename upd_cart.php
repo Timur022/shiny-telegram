6d4298a2107b9ecdb20ff6ab17f3ed8aa6159b33
Timur022 <61280530+Timur022@users.noreply.github.com>
2022-06-13 10:11:48 +0500

    Add files via upload

--- upd_cart.php
+++ upd_cart.php
@@ -6,6 +6,6 @@
     $product_cart = $us->fetch(PDO::FETCH_ASSOC);
 
     $dbh->prepare("UPDATE product SET quantity=? WHERE id=?")->execute([$product_cart['quantity']-$_GET['quantity'], $_COOKIE['product_id']]);
-    $dbh->prepare("INSERT INTO composition (amount, user, product, quantity) VALUES (?, ?, ?, ?)")->execute([$product_cart['price']*$_GET['quantity'], $_COOKIE['id'], $_GET['product_id'], $_GET['quantity']]);
+    $dbh->prepare("INSERT INTO composition (amount, \"user\", product, quantity) VALUES (?, ?, ?, ?)")->execute([$product_cart['price']*$_GET['quantity'], $_COOKIE['id'], $_GET['product_id'], $_GET['quantity']]);
     header('Location: cart.php');
  ?>
