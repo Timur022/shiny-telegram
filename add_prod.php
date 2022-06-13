34df350f81b5bf8cb358a9dd76474f8f8fdae5e0
Timur022 <61280530+Timur022@users.noreply.github.com>
2022-06-13 13:31:47 +0500

    Add files via upload

--- add_prod.php
+++ add_prod.php
@@ -1,10 +1,10 @@
 <?php
     require_once 'config.php';
-    try{
-	    $dbh->prepare("INSERT INTO 'product' ('name', 'brand', 'price', 'warranty', 'quantity', 'category', 'tags', 'about', 'specifications', 'foto') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")->execute([$_GET['name'], $_GET['brand'], $_GET['price'], $_GET['warranty'], $_GET['quantity'], $_GET['category'], $_GET['tags'], $_GET['about'], $_GET['specifications'], $_GET['foto']]);
-    } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage();
-        die();
-    }
+    // try{
+    $dbh->prepare("INSERT INTO 'product' ('name', 'brand', 'price', 'warranty', 'quantity', 'category', 'tags', 'about', 'specifications', 'foto') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")->execute([$_GET['name'], $_GET['brand'], $_GET['price'], $_GET['warranty'], $_GET['quantity'], $_GET['category'], $_GET['tags'], $_GET['about'], $_GET['specifications'], $_GET['foto']]);
+    // } catch (PDOException $e) {
+    //     print "Error!: " . $e->getMessage();
+    //     die();
+    // }
     header('Location: panel.php');
  ?>
