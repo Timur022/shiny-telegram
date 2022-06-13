86037a1f27a06ba9db0ff3919ad4faaffadface4
Timur022 <61280530+Timur022@users.noreply.github.com>
2022-06-13 09:02:03 +0500

    Add files via upload

--- single-product.php
+++ single-product.php
@@ -3,7 +3,7 @@
     $us = $dbh->prepare("SELECT * FROM product ORDER BY id DESC");
     $us->execute();
     $product = $us->fetchAll(PDO::FETCH_ASSOC);
-    $us = $dbh->prepare("SELECT * FROM user WHERE id = ".$_COOKIE['id']);
+    $us = $dbh->prepare("SELECT * FROM \"user\" WHERE id = ".$_COOKIE['id']);
     $us->execute();
     $user = $us->fetch(PDO::FETCH_ASSOC);
     $us_views = $user['last_view'];
